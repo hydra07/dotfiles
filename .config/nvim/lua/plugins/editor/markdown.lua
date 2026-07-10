c1e328674dfa7657e1893fe894809ac8a1a4796d
hydra07 <abcxyzkhong@gmail.com>
2026-07-10 17:01:28 +0700

    update linux

--- .config/nvim/lua/plugins/editor/markdown.lua
+++ .config/nvim/lua/plugins/editor/markdown.lua
@@ -1,22 +1,21 @@
 return {
 	{
-		dir = "D:/dev/markdown-kit.nvim/nvim/",
-		name = "markdown-kit",
+		"hydra07/markdown-kit.nvim",
+		branch = "runtime-linux-x64",
 		ft = { "markdown" },
-		cmd = {
-			"MarkdownKitStart",
-			"MarkdownKitStop",
-			"MarkdownKitToggle",
-		},
+		-- build = "mise trust .mise.toml && mise run setup && mise run build",
+		config = function()
+			-- Optional: custom root path if needed
+			-- vim.g.markdown_kit_root = vim.fn.stdpath("data") .. "/lazy/markdown-kit.nvim/"
+		end,
 	},
 	-- {
-	-- 	"hydra07/markdown-kit.nvim",
-	-- 	branch = "runtime-windows-x64",
-	-- 	ft = { "markdown" },
-	-- 	-- build = "mise trust .mise.toml && mise run setup && mise run build",
-	-- 	config = function()
-	-- 		-- Optional: custom root path if needed
-	-- 		-- vim.g.markdown_kit_root = vim.fn.stdpath("data") .. "/lazy/markdown-kit.nvim/"
+	-- 	"iamcco/markdown-preview.nvim",
+	-- 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	-- 	build = "cd app && bun i",
+	-- 	init = function()
+	-- 		vim.g.mkdp_filetypes = { "markdown" }
 	-- 	end,
+	-- 	ft = { "markdown" },
 	-- },
 }
